3c83819d165af001ea24d16a4ed5cd0882441f70
ahmadfouadalwakeal <ahmedfouad2592001@gmail.com>
2024-09-15 00:11:23 +0300

    modifiy name files

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_maps/core/routing/routes.dart';
-import 'package:flutter_maps/features/signin/ui/signin_screen.dart';
+import 'package:flutter_maps/features/login/ui/login_screen.dart';
 
 class AppRouter {
   Route? generateRoute(RouteSettings setting) {
     final arguments = setting.arguments;
     switch (setting.name) {
-      case Routes.signInScreen:
+      case Routes.loginScreen:
         return MaterialPageRoute(
-          builder: (_) => const SignInScreen(),
+          builder: (_) => const LoginScreen(),
         );
       default:
         return null;
